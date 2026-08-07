c56cd8199bc253615767a629c2c804dcb511027c
mk42 <miura.kouichi.p@gmail.com>
2026-08-07 17:44:37 +0900

    摩擦係数調整

--- Root/Root/Raven/Scene/SceneGame.h
+++ Root/Root/Raven/Scene/SceneGame.h
@@ -72,8 +72,8 @@ private:
 	Entity m_FloorEntity;
 
 	bool m_WasSpacePressed = false;
-	int m_MinSphereCount = 16;
-	int m_MaxSphereCount = 72;
+	int m_MinSphereCount = 50;
+	int m_MaxSphereCount = 100;
 	float m_TargetSphereDensity = 0.015f;
 
 	float m_Gravity = -9.8f;
