482dd9a008d2e765258b426cccf49fb6abbdab0c
kou42 <59990003+kou42@users.noreply.github.com>
2026-08-30 00:07:15 +0900

    Texture生成処理をOpenGL実装から分離

--- Root/Root/Raven/Renderer/Texture/Texture.cpp
+++ Root/Root/Raven/Renderer/Texture/Texture.cpp
@@ -1,13 +1,9 @@
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-
 #include "Raven/Renderer/Texture/Texture.h"
-#include <glad/glad.h>
-#include <string>
-#include <iostream>
 
-#include "Raven/Core/Base.h"
 #include "Raven/Renderer/RendererAPI.h"
+#include "Raven/Renderer/Texture/OpenGLTexture.h"
+
+#include <iostream>
 
 namespace Raven
 {
@@ -17,87 +13,23 @@ Ref<Texture> Texture::Create(const std::string& path)
     switch (RendererAPI::GetAPI())
     {
     case RendererAPI::API::OpenGL:
-        return CreateRef<Texture>(path);
+        return CreateRef<OpenGLTexture>(path);
 
     case RendererAPI::API::DirectX11:
-        // return CreateRef<DirectX11Shader>(filepath);
+        // 将来DirectX11Textureを追加した際に、ここで生成先を切り替えます。
         return nullptr;
+
     case RendererAPI::API::DirectX12:
-        // return CreateRef<DirectX12Shader>(filepath);
+        // 将来DirectX12Textureを追加した際に、ここで生成先を切り替えます。
         return nullptr;
     }
 
     return nullptr;
 }
 
-Texture::Texture(const std::string& path)
-{
-    glGenTextures(1, &m_ID);
-    glBindTexture(GL_TEXTURE_2D, m_ID);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    stbi_set_flip_vertically_on_load(true);
-
-    unsigned char* data = stbi_load(path.c_str(), &m_Width, &m_Height, &m_Channels, 0);
-
-    if (data)
-    {
-        GLenum format = GL_RGB;
-
-        if (m_Channels == 1) format = GL_RED;
-        else if (m_Channels == 3) format = GL_RGB;
-        else if (m_Channels == 4) format = GL_RGBA;
-
-        glTexImage2D(
-            GL_TEXTURE_2D,
-            0,
-            format,
-            m_Width,
-            m_Height,
-            0,
-            format,
-            GL_UNSIGNED_BYTE,
-            data
-        );
-
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else
-    {
-        std::cout << "Failed to load texture: " << path << std::endl;
-    }
-
-    stbi_image_free(data);
-}
-
-Texture::~Texture()
-{
-    glDeleteTextures(1, &m_ID);
-}
-
-void Texture::Bind(unsigned int slot) const
-{
-    glActiveTexture(GL_TEXTURE0 + slot);
-    glBindTexture(GL_TEXTURE_2D, m_ID);
-}
-
-void Texture::Unbind() const
-{
-    glBindTexture(GL_TEXTURE_2D, 0);
-}
-
-unsigned int Texture::GetID() const
-{
-    return m_ID;
-}
-
 void TextureLibrary::Add(const std::string& name, const Ref<Texture>& texture)
 {
-    if (!texture)
+    if (texture == nullptr)
     {
         std::cerr << "TextureLibrary::Add failed. Texture is null: " << name << std::endl;
         return;
