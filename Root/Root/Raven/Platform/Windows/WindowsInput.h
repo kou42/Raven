7d069eb756b0cade6e5e501729a88299b051ee64
kou42 <59990003+kou42@users.noreply.github.com>
2026-08-29 12:42:45 +0900

    Windows入力実装へゲームパッドAPIを追加

--- Root/Root/Raven/Platform/Windows/WindowsInput.h
+++ Root/Root/Raven/Platform/Windows/WindowsInput.h
@@ -16,6 +16,8 @@ protected:
     virtual bool IsKeyPressedImpl(int keycode) override;
     virtual bool IsMousePressedImpl(int button) override;
     virtual std::pair<float, float> GetMousePositionImpl() override;
+    virtual bool IsGamepadConnectedImpl(int gamepadIndex) override;
+    virtual bool GetGamepadStateImpl(int gamepadIndex, GamepadState& outState) override;
 
 private:
     GLFWwindow* m_Window = nullptr;
