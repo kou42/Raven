629ff734ca347bc6715bc7e0ddb5056cd6a6b55e
kou42 <59990003+kou42@users.noreply.github.com>
2026-08-31 00:24:28 +0900

    UIイベント型のコメントを整理

--- Root/Root/Raven/UI/Core/UIEvent.h
+++ Root/Root/Raven/UI/Core/UIEvent.h
@@ -28,6 +28,9 @@ enum class UIMouseEventType
 // UIContextからUIElement TreeへRoutingする最小マウスイベントです。
 // Targetは最初にHitしたElementを保持し、CurrentTargetはBubble中に現在処理している
 // Elementへ更新します。Handledをtrueにすると、それより上のParentへの伝播を停止できます。
+//
+// Hover / Pressed / Clickはこの生入力イベントの上に構築し、Hit TestやRouting自体には
+// Widget固有の状態を持たせない設計とします。
 struct UIMouseEvent
 {
     UIMouseEventType Type = UIMouseEventType::Move;
