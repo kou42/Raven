24ec88cded556970d7adfb58e9b7d3e56cda863e
kou42 <59990003+kou42@users.noreply.github.com>
2026-08-30 21:27:19 +0900

    OpenGL IndexBufferの動的更新を実装

--- Root/Root/Raven/Renderer/Buffer/OpenGLIndexBuffer.h
+++ Root/Root/Raven/Renderer/Buffer/OpenGLIndexBuffer.h
@@ -8,13 +8,12 @@ namespace Raven
 class OpenGLIndexBuffer : public IndexBuffer
 {
 public:
-
     OpenGLIndexBuffer(const uint32_t* indices, uint32_t count);
-
     virtual ~OpenGLIndexBuffer();
 
     virtual void Bind() const override;
     virtual void Unbind() const override;
+    virtual void SetData(const uint32_t* indices, uint32_t count) override;
 
     virtual uint32_t GetCount() const override
     {
@@ -22,9 +21,12 @@ public:
     }
 
 private:
+    uint32_t m_RendererID = 0;
+    uint32_t m_Count = 0;
 
-    uint32_t m_RendererID;
-    uint32_t m_Count;
+    // GPU側へ確保済みのindex容量です。
+    // UIのように毎frame要素数が変わる場合でも、容量内ならglBufferSubDataで再利用します。
+    uint32_t m_Capacity = 0;
 };
 
 }
