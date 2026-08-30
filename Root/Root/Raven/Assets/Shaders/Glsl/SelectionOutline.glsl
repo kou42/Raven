7c1480f18b6dc36ca94ead9b6f2fa4c1ecb6c030
kou42 <59990003+kou42@users.noreply.github.com>
2026-08-30 15:50:02 +0900

    Selection Outline用Editor Shaderを追加

---
+++ Root/Root/Raven/Assets/Shaders/Glsl/SelectionOutline.glsl
@@ -0,0 +1,28 @@
+#type vertex
+#version 330 core
+
+layout(location = 0) in vec3 a_Position;
+
+uniform mat4 u_Model;
+uniform mat4 u_View;
+uniform mat4 u_Projection;
+
+void main()
+{
+    gl_Position = u_Projection * u_View * u_Model * vec4(a_Position, 1.0);
+}
+
+#type fragment
+#version 330 core
+
+layout(location = 0) out vec4 o_Color;
+
+uniform vec4 u_OutlineColor;
+
+void main()
+{
+    // Selection OutlineはScene View表示用Color Attachment 0だけへ出力します。
+    // Picking用R32I Attachment(location = 1)へは何も書き込まないため、
+    // 直前のEntity Picking結果を壊しません。
+    o_Color = u_OutlineColor;
+}
