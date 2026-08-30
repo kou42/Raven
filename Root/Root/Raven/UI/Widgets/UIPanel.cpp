9b9894fa3bb80d37479ee8d62a65d4747eac6503
kou42 <59990003+kou42@users.noreply.github.com>
2026-08-31 02:15:01 +0900

    UIPanelのcpp実装を追加

---
+++ Root/Root/Raven/UI/Widgets/UIPanel.cpp
@@ -0,0 +1,30 @@
+#include "Raven/UI/Widgets/UIPanel.h"
+
+namespace Raven
+{
+
+void UIPanel::SetBackgroundColor(const math::Vec4& color)
+{
+    m_BackgroundColor = color;
+}
+
+const math::Vec4& UIPanel::GetBackgroundColor() const
+{
+    return m_BackgroundColor;
+}
+
+void UIPanel::OnBuildDrawList(
+    UIDrawList& drawList,
+    const math::Vec2& absolutePosition) const
+{
+    const math::Vec2& size = GetSize();
+
+    drawList.AddRect(
+        absolutePosition,
+        math::Vec2(
+            absolutePosition.x + size.x,
+            absolutePosition.y + size.y),
+        m_BackgroundColor);
+}
+
+} // namespace Raven
