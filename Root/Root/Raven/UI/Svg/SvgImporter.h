72ce884c9632c309262da1413f6c0efa0d20bac0
kou42 <59990003+kou42@users.noreply.github.com>
2026-09-06 04:11:49 +0900

    SvgImporter公開ヘッダからVectorDocument依存を除去

--- Root/Root/Raven/UI/Svg/SvgImporter.h
+++ Root/Root/Raven/UI/Svg/SvgImporter.h
@@ -1,12 +1,13 @@
 #pragma once
 
 #include "Raven/UI/Document/IDocumentImporter.h"
-#include "Raven/UI/Document/VectorDocument.h"
 
 #include <string>
 
 namespace Raven
 {
+
+struct VectorDocument;
 
 // SVG固有の構文解析を担当するImporterです。
 // 公開境界はUIDocumentへ統一し、Runtime側からSVG固有のParser構成を隠蔽します。
