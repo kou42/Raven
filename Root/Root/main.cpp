8181b572e9fc2f3e5681e95a824e5d01e2894413
kou42 <59990003+kou42@users.noreply.github.com>
2026-09-06 23:35:01 +0900

    Sprint Locomotion Self TestをDebug起動へ追加

--- Root/Root/main.cpp
+++ Root/Root/main.cpp
@@ -21,6 +21,7 @@
 #ifdef _DEBUG
 #include "Raven/Animation/Tests/BlendTreeRuntimeSelfTests.h"
 #include "Raven/Character/Tests/CharacterCeilingCollisionSelfTests.h"
+#include "Raven/Character/Tests/CharacterSprintLocomotionSelfTests.h"
 #include "Raven/Physics/Tests/SoftBodyIntegratedStepSelfTests.h"
 #include "Raven/UI/Svg/Debug/UISvgDemoLayer.h"
 #endif
@@ -39,6 +40,7 @@ int main()
     // Character locomotionの速度選択とBlendTree/Animation Profileの回帰テストも
     // 実際のDebug起動時に必ず通し、Sprint追加後の互換性退行を早い段階で検出します。
     Raven::tests::RunCharacterCeilingCollisionSelfTests();
+    Raven::tests::RunCharacterSprintLocomotionSelfTests();
     Raven::tests::RunBlendTreeRuntimeSelfTests();
     Raven::ph::tests::RunSoftBodyIntegratedStepSelfTests();
 
