57e093dce20b000d6f22cb4b3122863ff74501ac
kou42 <59990003+kou42@users.noreply.github.com>
2026-09-12 23:00:34 +0900

    GLM依存をMath基盤から削除

--- Root/Root/Raven/Math/Math.h
+++ Root/Root/Raven/Math/Math.h
@@ -1,7 +1,6 @@
 #pragma once
 
-#include <glm/glm.hpp>
-#include <glm/gtc/quaternion.hpp>
+#include <cmath>
 
 namespace Raven
 {
@@ -25,18 +24,5 @@ namespace math
     {
         return std::fabs(a - b) <= eps;
     }
-
-    /*
-    {
-        using Vec2 = glm::vec2;
-        using Vec3 = glm::vec3;
-        using Vec4 = glm::vec4;
-
-        using Mat3 = glm::mat3;
-        using Mat4 = glm::mat4;
-
-        using Quat = glm::quat;
-    }
-    */
 }
 }
