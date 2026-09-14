fa063d29c0533183b809339af63f83669c0a09bb
kou42 <59990003+kou42@users.noreply.github.com>
2026-09-14 23:32:24 +0900

    Fluid Coupling計測Self TestをDebug起動時に実行

--- Root/Root/main.cpp
+++ Root/Root/main.cpp
@@ -27,6 +27,7 @@
 #include "Raven/Animation/Tests/PoseInertializerSelfTests.h"
 #include "Raven/Character/Tests/CharacterCeilingCollisionSelfTests.h"
 #include "Raven/Character/Tests/CharacterSprintLocomotionSelfTests.h"
+#include "Raven/Physics/Tests/FluidCouplingMeasurementSelfTests.h"
 #include "Raven/Physics/Tests/FluidWorldSelfTests.h"
 #include "Raven/Physics/Tests/PhysicsSimulationWorldSelfTests.h"
 #include "Raven/Physics/Tests/SoftBodyIntegratedStepSelfTests.h"
@@ -54,6 +55,7 @@ int main()
     Raven::tests::RunPoseInertializerSelfTests();
     Raven::ph::tests::RunPhysicsSimulationWorldSelfTests();
     Raven::ph::tests::RunFluidWorldSelfTests();
+    Raven::ph::tests::RunFluidCouplingMeasurementSelfTests();
     Raven::ph::tests::RunSoftBodyIntegratedStepSelfTests();
     Raven::ph::tests::RunStaticMeshTriangleBVHSelfTests();
     Raven::ph::tests::RunThermalWorldSelfTests();
