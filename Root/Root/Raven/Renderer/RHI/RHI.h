f1bc987cd4aeda0f1fa9f13d855ecdf455fa6b5b
kou42 <59990003+kou42@users.noreply.github.com>
2026-09-17 00:21:32 +0900

    RHI統合ヘッダへTextureを追加

--- Root/Root/Raven/Renderer/RHI/RHI.h
+++ Root/Root/Raven/Renderer/RHI/RHI.h
@@ -2,6 +2,7 @@
 
 #include "Raven/Renderer/RHI/RHIDevice.h"
 #include "Raven/Renderer/RHI/RHIBuffer.h"
+#include "Raven/Renderer/RHI/RHITexture.h"
 #include "Raven/Renderer/RHI/RHICommandList.h"
 #include "Raven/Renderer/RHI/RHIResource.h"
 #include "Raven/Renderer/RHI/RHITypes.h"
