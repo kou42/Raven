647a9deff18adcbd5b600d7e0acd9e35bd8d1f41
kou42 <59990003+kou42@users.noreply.github.com>
2026-09-17 00:50:24 +0900

    OpenGL RHIでViewport参照を実装

--- Root/Root/Raven/Platform/OpenGL/RHI/OpenGLRHICommandList.h
+++ Root/Root/Raven/Platform/OpenGL/RHI/OpenGLRHICommandList.h
@@ -11,6 +11,7 @@ public:
     ~OpenGLRHICommandList() override = default;
 
     void SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height) override;
+    RHIViewport GetViewport() const override;
     void SetClearColor(float r, float g, float b, float a) override;
     void Clear() override;
 
