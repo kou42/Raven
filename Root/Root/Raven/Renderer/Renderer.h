4bec2f50b753a6b4d1577f548549054cd6683772
kou42 <59990003+kou42@users.noreply.github.com>
2026-09-17 01:31:20 +0900

    RendererからLegacy GetAPIを削除

--- Root/Root/Raven/Renderer/Renderer.h
+++ Root/Root/Raven/Renderer/Renderer.h
@@ -12,7 +12,6 @@ namespace Raven
 class Camera;
 class Material;
 class Mesh;
-class RendererAPI;
 class Shader;
 class VertexArray;
 
@@ -71,8 +70,6 @@ public:
     static void Submit(const Ref<Shader>& shader, const Ref<VertexArray>& vertexArray);
     static void DrawIndexed(const Ref<VertexArray>& vertexArray);
     static void Draw(const Ref<Mesh>& mesh, const Ref<Material>& material, const math::Mat4& transform);
-
-    static RendererAPI& GetAPI();
 
     static const RendererStatistics& GetStatistics();
     static void RecordIndexedDraw(uint32_t indexCount, PrimitiveTopology topology);
