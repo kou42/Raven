ada6386c4cc4e8d8a6edd35d5cb4d19acdf5a23b
kou42 <59990003+kou42@users.noreply.github.com>
2026-09-16 23:10:33 +0900

    OpenGL RHI CommandListを追加

---
+++ Root/Root/Raven/Platform/OpenGL/RHI/OpenGLRHICommandList.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Raven/Renderer/RHI/RHICommandList.h"
+
+namespace Raven
+{
+
+class OpenGLRHICommandList final : public RHICommandList
+{
+public:
+    ~OpenGLRHICommandList() override = default;
+
+    void SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height) override;
+    void SetClearColor(float r, float g, float b, float a) override;
+    void Clear() override;
+    void DrawIndexed(const Ref<VertexArray>& vertexArray, uint32_t indexCount = 0) override;
+};
+
+} // namespace Raven
