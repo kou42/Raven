62b44c4289ae204745780a9003044a8ceac1ffd1
kou42 <59990003+kou42@users.noreply.github.com>
2026-09-17 01:30:24 +0900

    OpenGL RHI CommandList初期化を実装

--- Root/Root/Raven/Platform/OpenGL/RHI/OpenGLRHICommandList.h
+++ Root/Root/Raven/Platform/OpenGL/RHI/OpenGLRHICommandList.h
@@ -10,6 +10,7 @@ class OpenGLRHICommandList final : public RHICommandList
 public:
     ~OpenGLRHICommandList() override = default;
 
+    void Init() override;
     void SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height) override;
     RHIViewport GetViewport() const override;
     void SetClearColor(float r, float g, float b, float a) override;
