46965658fb0df8cbb3e3f82278ede3ef987dc8ee
kou42 <59990003+kou42@users.noreply.github.com>
2026-09-16 23:12:25 +0900

    OpenGL CommandListの初期責務をClear系へ限定

--- Root/Root/Raven/Platform/OpenGL/RHI/OpenGLRHICommandList.h
+++ Root/Root/Raven/Platform/OpenGL/RHI/OpenGLRHICommandList.h
@@ -13,7 +13,6 @@ public:
     void SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height) override;
     void SetClearColor(float r, float g, float b, float a) override;
     void Clear() override;
-    void DrawIndexed(const Ref<VertexArray>& vertexArray, uint32_t indexCount = 0) override;
 };
 
 } // namespace Raven
