3cd362cdbb7ba802778b2eb8a240eb8f4bcf7b9f
kou42 <59990003+kou42@users.noreply.github.com>
2026-09-16 23:10:58 +0900

    RHI公開ヘッダーにCommandListを追加

--- Root/Root/Raven/Renderer/RHI/RHI.h
+++ Root/Root/Raven/Renderer/RHI/RHI.h
@@ -2,5 +2,6 @@
 
 #include "Raven/Renderer/RHI/RHIDevice.h"
 #include "Raven/Renderer/RHI/RHIBuffer.h"
+#include "Raven/Renderer/RHI/RHICommandList.h"
 #include "Raven/Renderer/RHI/RHIResource.h"
 #include "Raven/Renderer/RHI/RHITypes.h"
