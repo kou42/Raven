8d2f3038b0e0af808b01305a90ac8cc59ef35d01
kou42 <59990003+kou42@users.noreply.github.com>
2026-09-17 00:50:45 +0900

    OpenGL RHIで現在のViewportを取得

--- Root/Root/Raven/Platform/OpenGL/RHI/OpenGLRHICommandList.cpp
+++ Root/Root/Raven/Platform/OpenGL/RHI/OpenGLRHICommandList.cpp
@@ -42,6 +42,19 @@ void OpenGLRHICommandList::SetViewport(uint32_t x, uint32_t y, uint32_t width, u
         static_cast<GLsizei>(height));
 }
 
+RHIViewport OpenGLRHICommandList::GetViewport() const
+{
+    GLint viewport[4] = {};
+    glGetIntegerv(GL_VIEWPORT, viewport);
+
+    RHIViewport result{};
+    result.X = viewport[0] > 0 ? static_cast<uint32_t>(viewport[0]) : 0u;
+    result.Y = viewport[1] > 0 ? static_cast<uint32_t>(viewport[1]) : 0u;
+    result.Width = viewport[2] > 0 ? static_cast<uint32_t>(viewport[2]) : 0u;
+    result.Height = viewport[3] > 0 ? static_cast<uint32_t>(viewport[3]) : 0u;
+    return result;
+}
+
 void OpenGLRHICommandList::SetClearColor(float r, float g, float b, float a)
 {
     glClearColor(r, g, b, a);
