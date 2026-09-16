92f5b53522af0b31fff320c327e9f462db046354
kou42 <59990003+kou42@users.noreply.github.com>
2026-09-17 01:46:48 +0900

    OpenGL VertexArray実装をPlatform層へ移動

---
+++ Root/Root/Raven/Platform/OpenGL/OpenGLVertexArray.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Raven/Renderer/Buffer/VertexArray.h"
+//#include "../../Core/Base.h"
+
+namespace Raven
+{
+
+// OpenGL固有のVAO実装はPlatform層へ閉じ込めます。
+// Renderer/Buffer側はAPI非依存interfaceとfactoryだけを保持します。
+class OpenGLVertexArray : public VertexArray
+{
+public:
+    OpenGLVertexArray();
+    virtual ~OpenGLVertexArray();
+
+    virtual void Bind() const override;
+    virtual void Unbind() const override;
+
+    virtual void AddVertexBuffer(const Ref<VertexBuffer>& vertexBuffer) override;
+
+    virtual void SetIndexBuffer(const Ref<IndexBuffer>& indexBuffer) override;
+
+    virtual const Ref<IndexBuffer>& GetIndexBuffer() const override
+    {
+        return m_IndexBuffer;
+    }
+
+private:
+    uint32_t m_RendererID;
+
+    std::vector<Ref<VertexBuffer>> m_VertexBuffers;
+    Ref<IndexBuffer> m_IndexBuffer;
+};
+
+}
