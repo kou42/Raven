e4a62b194b68752b52c7f35728d626841acc44e8
kou42 <59990003+kou42@users.noreply.github.com>
2026-09-20 02:55:27 +0900

    OpenGL Clear ContextにFrame段階と状態を追加

--- Root/Root/Raven/Platform/OpenGL/OpenGLClearContext.h
+++ Root/Root/Raven/Platform/OpenGL/OpenGLClearContext.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Raven/Renderer/RHI/RHIClearContext.h"
+#include "Raven/Renderer/RHI/RHIFrameLifecycle.h"
 
 struct GLFWwindow;
 
@@ -8,15 +9,21 @@ namespace Raven
 {
 // 既存OpenGL Applicationとは独立したClear検証経路です。
 // OpenGL Context自体はWindowsWindowが所有するため、ここでは借用だけします。
-class OpenGLClearContext : public RHIClearContext
+class OpenGLClearContext : public RHIClearContext, public RHIFrameLifecycle
 {
 public:
     bool Init(Window& window) override;
     RHIFrameResult DrawClearFrame(const float clearColor[4]) override;
+    RHIFrameResult BeginFrame() override;
+    RHIFrameResult ClearFrame(const float clearColor[4]) override;
+    RHIFrameResult EndFrame() override;
+    RHIFrameResult Present() override;
     bool Resize(uint32_t width, uint32_t height) override;
     void Shutdown() override;
 
 private:
     GLFWwindow* m_Window = nullptr;
+    bool m_FrameActive = false;
+    bool m_FrameEnded = false;
 };
 } // namespace Raven
