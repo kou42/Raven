5850ca513ca0cfdd5138f63e047cf9522db3a752
kou42 <59990003+kou42@users.noreply.github.com>
2026-09-19 23:53:22 +0900

    DX12初期化・描画・Resize時にInfoQueue診断を出力

--- Root/Root/Raven/Platform/DirectX12/DX12ClearContext.cpp
+++ Root/Root/Raven/Platform/DirectX12/DX12ClearContext.cpp
@@ -70,6 +70,7 @@ bool DX12ClearContext::Init(Window& window)
         }
         m_Frames.push_back(std::move(frame));
     }
+    m_Device.DrainDebugMessages();
     m_CurrentFrame = 0;
     m_VSync = window.IsVSync();
     return true;
@@ -87,6 +88,7 @@ bool DX12ClearContext::DrawClearFrame(const float clearColor[4])
     {
         return false;
     }
+    m_Device.DrainDebugMessages();
     m_CurrentFrame = (m_CurrentFrame + 1) % static_cast<uint32_t>(m_Frames.size());
     return true;
 }
@@ -105,7 +107,9 @@ bool DX12ClearContext::Resize(uint32_t width, uint32_t height)
     {
         return false;
     }
-    return m_FrameRenderer.RebuildRenderTargets(m_Device.GetHandle(), m_SwapChain);
+    const bool rebuilt = m_FrameRenderer.RebuildRenderTargets(m_Device.GetHandle(), m_SwapChain);
+    m_Device.DrainDebugMessages();
+    return rebuilt;
 }
 
 void DX12ClearContext::Shutdown()
