f9d4c518f6f4e3165e8f1eb3d7ae4b718b625688
kou42 <59990003+kou42@users.noreply.github.com>
2026-09-22 01:15:23 +0900

    WindowにIME候補位置の問い合わせCallbackを追加

--- Root/Root/Raven/Core/Window.h
+++ Root/Root/Raven/Core/Window.h
@@ -31,6 +31,7 @@ class Window
 {
 public:
     using EventCallbackFn = std::function<void(Event&)>;
+    using IMECaretPositionFn = std::function<bool(float&, float&)>;
 
     virtual ~Window() = default;
 
@@ -49,6 +50,8 @@ public:
     virtual void* GetPlatformWindowHandle() const = 0;
 
     virtual void SetEventCallback(const EventCallbackFn& callback) = 0;
+    // GLFW論理Window座標でCaret位置を取得します。非対応Platformは既定で無視します。
+    virtual void SetIMECaretPositionCallback(IMECaretPositionFn callback) { (void)callback; }
     virtual void SetVSync(bool enabled) = 0;
     virtual bool IsVSync() const = 0;
 
