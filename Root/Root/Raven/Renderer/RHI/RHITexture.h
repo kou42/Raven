1e3e1f6b5635d60b30d556d6e181897f743b6349
kou42 <59990003+kou42@users.noreply.github.com>
2026-09-23 02:39:51 +0900

    Vulkan等の既存RHI Texture実装を壊さず診断APIを段階導入

--- Root/Root/Raven/Renderer/RHI/RHITexture.h
+++ Root/Root/Raven/Renderer/RHI/RHITexture.h
@@ -49,7 +49,12 @@ public:
 
     virtual void SetData(const void* data, std::size_t dataSize) = 0;
     // 明示診断経路です。既存のvoid SetData呼び出しは維持します。
-    virtual bool TrySetData(const void* data, std::size_t dataSize) = 0;
+    virtual bool TrySetData(const void* data, std::size_t dataSize)
+    {
+        (void)data;
+        (void)dataSize;
+        return false;
+    }
     virtual const RHITextureSpecification& GetSpecification() const = 0;
 };
 
