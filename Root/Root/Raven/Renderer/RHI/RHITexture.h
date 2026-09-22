f951708c1cd9a4dd739e45e3784bfabe2d6f97ab
kou42 <59990003+kou42@users.noreply.github.com>
2026-09-23 02:38:58 +0900

    RHI Textureへ診断付きピクセル転送APIを追加

--- Root/Root/Raven/Renderer/RHI/RHITexture.h
+++ Root/Root/Raven/Renderer/RHI/RHITexture.h
@@ -48,6 +48,8 @@ public:
     ~RHITexture() override = default;
 
     virtual void SetData(const void* data, std::size_t dataSize) = 0;
+    // 明示診断経路です。既存のvoid SetData呼び出しは維持します。
+    virtual bool TrySetData(const void* data, std::size_t dataSize) = 0;
     virtual const RHITextureSpecification& GetSpecification() const = 0;
 };
 
