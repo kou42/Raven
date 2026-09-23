e3ef03d08298fc1e5f56f11a86e8e5ca196d2ea8
kou42 <59990003+kou42@users.noreply.github.com>
2026-09-23 23:08:34 +0900

    Scissorの負座標を保持してUI描画後に正確に復元

--- Root/Root/Raven/Platform/OpenGL/RHI/OpenGLRHICommandList.h
+++ Root/Root/Raven/Platform/OpenGL/RHI/OpenGLRHICommandList.h
@@ -17,7 +17,7 @@ public:
     void RestoreRenderTargetState(const RHIRenderTargetState& state) override;
     void SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height) override;
     RHIViewport GetViewport() const override;
-    void SetScissor(bool enabled, uint32_t x, uint32_t y, uint32_t width, uint32_t height) override;
+    void SetScissor(bool enabled, int32_t x, int32_t y, uint32_t width, uint32_t height) override;
     RHIScissor GetScissor() const override;
     void SetClearColor(float r, float g, float b, float a) override;
     void Clear() override;
