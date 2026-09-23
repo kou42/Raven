0d494e0d5fbcc589877d2f418f0fc7cbdca4b784
kou42 <59990003+kou42@users.noreply.github.com>
2026-09-23 22:50:43 +0900

    UI向けScissor命令をRHIとOpenGL Backendへ追加

--- Root/Root/Raven/Platform/OpenGL/RHI/OpenGLRHICommandList.h
+++ Root/Root/Raven/Platform/OpenGL/RHI/OpenGLRHICommandList.h
@@ -13,6 +13,7 @@ public:
     void Init() override;
     void SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height) override;
     RHIViewport GetViewport() const override;
+    void SetScissor(bool enabled, uint32_t x, uint32_t y, uint32_t width, uint32_t height) override;
     void SetClearColor(float r, float g, float b, float a) override;
     void Clear() override;
 
