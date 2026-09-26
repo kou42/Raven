960933c83d3a52159bd5828f8634985fe4e40ed1
kou42 <59990003+kou42@users.noreply.github.com>
2026-09-26 21:35:03 +0900

    Game Sceneへ遷移要求用Application参照を接続

--- Root/Root/main.cpp
+++ Root/Root/main.cpp
@@ -174,7 +174,7 @@ int main(int argc, char* argv[])
         "Game",
         []()
         {
-            return Raven::CreateScope<Raven::SceneGame>();
+            return Raven::CreateScope<Raven::SceneGame>(&app);
         });
     const bool titleSceneRegistered = app.RegisterScene(
         "Title",
